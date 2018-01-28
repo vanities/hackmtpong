ac494588ebde7990b66590513fcf65ad29963ac4
Adamant Mischke <mischkeaa@gmail.com>
2018-01-28 01:19:55 -0600

    fixed a whole bunch of stuff

--- PongHackMT.py
+++ PongHackMT.py
@@ -14,6 +14,9 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
 
+import datetime
+import time
+
 class DQNAgent:
     def __init__(self, state_size, action_size):
         self.state_size = state_size
@@ -66,22 +69,26 @@ class DQNAgent:
         self.model.save_weights(name)
 
 
-#initialise the pygame module
+# initialise the pygame module
 pygame.init()
 
+# setting windows detail
 window_width = 500
 window_height = 500
 ScoreBarHeight = 30
 
+# colors
 white = (255, 255, 255)
 black = (0, 0, 0)
 
 
 # set up display size
 windowDisplay = pygame.display.set_mode((window_width,window_height), HWSURFACE | DOUBLEBUF | RESIZABLE)
-#Title
+
+# title
 pygame.display.set_caption("PongHackMT")
 
+# 
 ball_h = 9
 ball_w = 9
 paddleP_h = 45
@@ -102,7 +109,20 @@ def paddle2(paddleC_x,paddleC_y):
         windowDisplay.blit(paddle2_img,(paddleC_x,paddleC_y))
 def ball(ball_x,ball_y):
         windowDisplay.blit(ball_img, (ball_x,ball_y))
+def angleCalc(paddle_y, ball_y):
+        y =  5* ( (ball_y - (paddle_y + (paddleC_h / 2 ))) / paddleC_h*.5 )
+        return y
 
+## hard exits the game
+def quit(agent):
+    # save the model
+    fn = 'weights/pong_weights-' + str(datetime.datetime.now().strftime("%y-%m-%d-%H-%M")) \
+         + '.h5'
+    agent.save(fn)
+    print('Saved pong weights as',fn)
+    print('Exiting..')
+    pygame.quit()
+    sys.exit()
 
 paddleC_x = window_width - 10 - paddleC_w
 paddleP_x = 10
@@ -123,76 +143,85 @@ paddle_shift_rate=0.6
 
 myFont = pygame.font.SysFont("Courier New", 20, bold=True)
 
-state_size=8
-action_size=3
+
+# instantiate the Deep Q Neural Agent
+state_size = 8
+action_size = 3
 agent = DQNAgent(state_size, action_size)
-batch_size = 10
-EPISODES = 10
-curr_reward = 0
-total_reward=0
 
-def angleCalc(paddle_y, ball_y):
-        y =  5* ( (ball_y - (paddle_y + (paddleC_h / 2 ))) / paddleC_h*.5 )
-        return y
+# kinda large
+batch_size = 1000
 
+# total rewards throughout the lifetime of the game
+total_reward = 0
 
-gameExit = False
+# how many clocks until exit
+epoch = 300000000000
+
+# flag for training mode
 TRAINING = True
 
+# deque for the mean of the rewards measured in the matches
 mean = deque(maxlen=200000)
 
 print('hackmt pong ai: Training Mode', TRAINING)
 
-#gameloop
-while not gameExit:
+# game loop
+while epoch > 0:
 
-        if gameExit % 100:
-           print ('mean: ', np.mean(mean))
+    # current reward for the match
+    curr_reward = 0
 
+    if epoch != 3000000 and epoch % 100 == 0:
+       print ('mean: ', np.mean(mean), 'epoch', epoch)
+
+    if not TRAINING:
         scoresLine = pygame.draw.rect(windowDisplay, white, (0, ScoreBarHeight-1, window_width, 2), 0)
 
-        while ball_yspeed == 0:
-                ball_yspeed = random.uniform(-3,3)
+    while ball_yspeed == 0:
+            ball_yspeed = random.uniform(-3,3)
 
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
 
-        state = np.array([paddleP_y,paddleP_change, paddleC_y, paddleC_change,
-                            ball_x, ball_y, ball_xspeed, ball_yspeed])
-        state = np.reshape(state, [1, state_size])
-        action = agent.act(state)
+    state = np.array([paddleP_y,paddleP_change, paddleC_y, paddleC_change,
+                        ball_x, ball_y, ball_xspeed, ball_yspeed])
+    state = np.reshape(state, [1, state_size])
+    action = agent.act(state)
 
-        if action == 0:
-            paddleC_change = - (paddle_speed)
-        # down
-        if action == 2:
-            paddleC_change = (paddle_speed)
+    if action == 0:
+        paddleC_change = - (paddle_speed)
+    # down
+    if action == 2:
+        paddleC_change = (paddle_speed)
 
-        done = ball_x<0
-        if done:
-            print('computer scored')
-        if len(agent.memory) > batch_size:
-            agent.replay(batch_size)
+    done = ball_x<0
+    if done:
+        print('computer scored')
+    if len(agent.memory) > batch_size:
+        agent.replay(batch_size)
 
-        #Paddle Movement
+    #Paddle Movement
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            quit(agent)
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                quit(agent)
             if event.key == pygame.K_UP:
-                    paddleP_change = - (paddle_speed)
+                paddleP_change = - (paddle_speed)
             if event.key == pygame.K_DOWN:
-                    paddleP_change = (paddle_speed)
+                paddleP_change = (paddle_speed)
             if event.key == pygame.K_w:
-                    paddleC_change = - (paddle_speed)
+                paddleC_change = - (paddle_speed)
             if event.key == pygame.K_s:
-                    paddleC_change = (paddle_speed)
+                paddleC_change = (paddle_speed)
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    paddleP_change = 0
+                paddleP_change = 0
             if event.key == pygame.K_w or event.key == pygame.K_s:
-                    paddleC_change = 0
-        
-        #randomize cpu level
+                paddleC_change = 0
+    
+    #randomize cpu level
+    if not TRAINING:
         if paddle_shift_rate >= 1:
             paddle_shift_rate -= 0.7
         if paddle_shift > paddleP_h:
@@ -204,112 +233,108 @@ while not gameExit:
             paddleP_change = paddle_shift_rate * (paddle_speed)
 
 
-        # bounding box for the paddles
-        if paddleP_y + (paddleP_change+paddleP_h) >= window_height+paddle_speed or paddleP_y + (paddleP_change) <= ScoreBarHeight:
-            paddleP_change = 0
-        if paddleC_y + (paddleC_change+paddleC_h) >= window_height+paddle_speed or paddleC_y + (paddleC_change) <= ScoreBarHeight:
-            paddleC_change = 0
-        #END Paddle Movement
+    # bounding box for the paddles
+    if paddleP_y + (paddleP_change+paddleP_h) >= window_height+paddle_speed or paddleP_y + (paddleP_change) <= ScoreBarHeight:
+        paddleP_change = 0
+    if paddleC_y + (paddleC_change+paddleC_h) >= window_height+paddle_speed or paddleC_y + (paddleC_change) <= ScoreBarHeight:
+        paddleC_change = 0
+    #END Paddle Movement
 
 
-        #Ball Movement
-        paddleP_y += paddleP_change
-        paddleC_y += paddleC_change
-        ball_x += ball_xspeed
+    #Ball Movement
+    paddleP_y += paddleP_change
+    paddleC_y += paddleC_change
+    ball_x += ball_xspeed
+
+    if not TRAINING:
         pygame.display.update()
         windowDisplay.fill(black)
-        paddle1(paddleP_x,paddleP_y)
-        paddle2(paddleC_x,paddleC_y)
-        #END Ball Movement
+
+    paddle1(paddleP_x,paddleP_y)
+    paddle2(paddleC_x,paddleC_y)
+    #END Ball Movement
 
 
-        #Ball/Paddle Collision
-        
-        #Player Paddle
-        if ball_x + ball_xspeed <= paddleP_x + paddleP_w - 1 and ball_x + ball_w - 1 + ball_xspeed >= paddleP_x:
-            if ball_y + ball_yspeed <= paddleP_y + paddleP_h - 1 and ball_y + ball_h - 1 + ball_yspeed >= paddleP_y:
-                ball_x +=1
-                ball_xspeed *= -1
-                angle = angleCalc(paddleP_y, ball_y)
-                ball_yspeed = ball_xspeed * math.sin(angle)*2
-                player_hit = 1
+    # Ball/Paddle Collision
+    #Player Paddle
+    if ball_x + ball_xspeed <= paddleP_x + paddleP_w - 1 and ball_x + ball_w - 1 + ball_xspeed >= paddleP_x:
+        if ball_y + ball_yspeed <= paddleP_y + paddleP_h - 1 and ball_y + ball_h - 1 + ball_yspeed >= paddleP_y:
+            ball_x +=1
+            ball_xspeed *= -1
+            angle = angleCalc(paddleP_y, ball_y)
+            ball_yspeed = ball_xspeed * math.sin(angle)*2
+            player_hit = 1
+            
+            if TRAINING:
                 paddle_shift += 3
                 paddle_shift_rate += 0.08
 
-        #CPU paddle
-        if ball_x + ball_xspeed <= paddleC_x + paddleC_w - 1 and ball_x + ball_w - 1 + ball_xspeed >= paddleC_x:
-            if ball_y + ball_yspeed <= paddleC_y + paddleC_h - 1 and ball_y + ball_h - 1 + ball_yspeed >= paddleC_y:
-                ball_x -= 1
-                ball_xspeed *= -1
-                angle = angleCalc(paddleC_y, ball_y)
-                ball_yspeed = ball_xspeed * math.sin(angle) *-2
-                curr_reward = 1
-                computer_hit = 1
-                    
-        #END Ball/Paddle Collision
+    # CPU paddle
+    if ball_x + ball_xspeed <= paddleC_x + paddleC_w - 1 and ball_x + ball_w - 1 + ball_xspeed >= paddleC_x:
+        if ball_y + ball_yspeed <= paddleC_y + paddleC_h - 1 and ball_y + ball_h - 1 + ball_yspeed >= paddleC_y:
+            ball_x -= 1
+            ball_xspeed *= -1
+            angle = angleCalc(paddleC_y, ball_y)
+            ball_yspeed = ball_xspeed * math.sin(angle) *-2
+            computer_hit = 1    
+
+            # advance the current reward to 1
+            curr_reward = 1        
+    # END Ball/Paddle Collision
 
 
-        #Ball Out of Bounds
-        #If Player Loses
-        if (ball_x<0):
-            ball_x = 0.5 * window_width
-            ball_y = (0.5 * (window_height-ScoreBarHeight))+ScoreBarHeight
-            #ball_xspeed *= 
-            ball_yspeed = random.uniform(-3,3)
-            cpuScore += 1
-            playerTreat = -2
+    # Ball Out of Bounds
+    # If Player Loses
+    if (ball_x<0):
 
-        #If CPU Loses
-        if (ball_x>window_width):
-            ball_x = 0.5 * window_width
-            ball_y = (0.5 * (window_height-ScoreBarHeight))+ScoreBarHeight
-            #ball_xspeed = -1
-            ball_yspeed = random.uniform(-3,3)
-            playerScore += 1
-            cpuTreat = -2
-        #When Score reaches 20
-        if not TRAINING and playerScore == 20:
-            pygame.quit()
-            sys.exit()
-        #END Ball Out of Bounds
+        # reset the position of the player paddle
+        ball_x = 0.5 * window_width
+        ball_y = (0.5 * (window_height-ScoreBarHeight))+ScoreBarHeight
+        ball_yspeed = random.uniform(-3,3)
+        cpuScore += 1   # increase the scoreboard
 
+    # If CPU Loses
+    if (ball_x>window_width):
 
+        # reset the position of the cpu paddle
+        ball_x = 0.5 * window_width
+        ball_y = (0.5 * (window_height-ScoreBarHeight))+ScoreBarHeight
+        ball_yspeed = random.uniform(-3,3)
+        playerScore += 1    # increase the scoreboard
 
-        #Ball Vertical Limit
-        if ball_y  + ball_yspeed <= ScoreBarHeight - 1:
-                ball_y += (ScoreBarHeight-ball_y)-ball_yspeed
-                ball_yspeed = -1* ball_yspeed
-        elif ball_y + (ball_h-1) +ball_yspeed >= window_height:
-                ball_y += (window_height-(ball_y+ball_h-1))-ball_yspeed
-                ball_yspeed = -1* ball_yspeed
-        else:
-                ball_y += ball_yspeed
-        ball(ball_x,ball_y)
-        #END Ball Vertical Limit
+    # exit the match
+    if not TRAINING and playerScore == 20:
+        pygame.quit()
+        sys.exit()
+    # END Ball Out of Bounds
 
 
-        # Update and Display Score
+
+    # Ball Vertical Limit
+    if ball_y  + ball_yspeed <= ScoreBarHeight - 1:
+        ball_y += (ScoreBarHeight-ball_y)-ball_yspeed
+        ball_yspeed = -1* ball_yspeed
+    elif ball_y + (ball_h-1) +ball_yspeed >= window_height:
+        ball_y += (window_height-(ball_y+ball_h-1))-ball_yspeed
+        ball_yspeed = -1* ball_yspeed
+    else:
+        ball_y += ball_yspeed
+    #END Ball Vertical Limit
+
+    # set the coordinated of the ball
+    ball(ball_x,ball_y)
+
+    # Update and Display Score
+    if not TRAINING:
         cpuScoreDisplay = myFont.render(str(cpuScore), 1, white)
         playerScoreDisplay = myFont.render(str(playerScore), 1, white)
         windowDisplay.blit(cpuScoreDisplay, (window_width*3/4, ScoreBarHeight/2 - 10))
         windowDisplay.blit(playerScoreDisplay, (window_width/4, ScoreBarHeight/2 - 10))
-        #END Update and Display Score
+    # END Update and Display Score
 
-        if not TRAINING:
-            clock.tick(30)
+    if not TRAINING:
+        clock.tick(30)
 
-
-        #if player_hit:
-        # print('player hit')
-
-        #if player_scored:
-        # print('player scored')
-        # total_reward-=10
-        #if computer_scored:
-        # print('computer scored')
-        # total_reward+=10
-
-        mean.append(curr_reward)
-
-if gameExit:
-    model.save('pong_model.h5')
+    # append the current reward value to the mean deque
+    mean.append(curr_reward)
+    epoch-=1     # reduce the game ticker down one
