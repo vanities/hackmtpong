b950e3211eac3a48ce44234fbda2800d0f5cd84d
Adamant Mischke <mischkeaa@gmail.com>
2018-01-29 14:21:00 -0600

    seperated the agent from the game

---
+++ DQNAgent.py
@@ -0,0 +1,60 @@
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.models import load_model
+import numpy as np
+import random
+
+
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=20000)
+        self.gamma = 0.999 # discount rate
+        self.epsilon = 0.1  # exploration rate
+        self.epsilon_min = 0.001
+        self.epsilon_decay = 0.999999
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+        self.model.load_weights('pong_weights-18-01-28-16-18.h5')
+
+    def _build_model(self):
+        # Neural Net for Deep-Q learning Model
+        model = Sequential()
+        model.add(Dense(128, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse',
+        optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0]) # returns action
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+        if not done:
+            target = (reward + self.gamma *
+            np.amax(self.model.predict(next_state)[0]))
+        #print(state)
+        target_f = self.model.predict(state)
+        target_f[0][action] = target
+        self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
--- PongHackMT.py
+++ PongHackMT.py
@@ -9,64 +9,14 @@ import gym
 import numpy as np
 import pygame, sys, random
 from pygame.locals import *
-from collections import deque
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
+from DQNAgent import *
 
 import datetime
 import time
 
-class DQNAgent:
-    def __init__(self, state_size, action_size):
-        self.state_size = state_size
-        self.action_size = action_size
-        self.memory = deque(maxlen=20000)
-        self.gamma = 0.999 # discount rate
-        self.epsilon = 0.1  # exploration rate
-        self.epsilon_min = 0.001
-        self.epsilon_decay = 0.999999
-        self.learning_rate = 0.001
-        self.model = self._build_model()
+# flag for training mode
+TRAINING = False
 
-    def _build_model(self):
-        # Neural Net for Deep-Q learning Model
-        model = Sequential()
-        model.add(Dense(128, input_dim=self.state_size, activation='relu'))
-        model.add(Dense(128, activation='relu'))
-        model.add(Dense(self.action_size, activation='linear'))
-        model.compile(loss='mse',
-        optimizer=Adam(lr=self.learning_rate))
-        return model
-
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
-
-    def act(self, state):
-        if np.random.rand() <= self.epsilon:
-            return random.randrange(self.action_size)
-        act_values = self.model.predict(state)
-        return np.argmax(act_values[0]) # returns action
-
-    def replay(self, batch_size):
-        minibatch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in minibatch:
-            target = reward
-        if not done:
-            target = (reward + self.gamma *
-            np.amax(self.model.predict(next_state)[0]))
-        #print(state)
-        target_f = self.model.predict(state)
-        target_f[0][action] = target
-        self.model.fit(state, target_f, epochs=1, verbose=0)
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
-
-    def load(self, name):
-        self.model.load_weights(name)
-
-    def save(self, name):
-        self.model.save_weights(name)
 
 
 # initialise the pygame module
@@ -159,8 +109,6 @@ total_reward = 0
 epoch = 0
 TOTAL_TICKS = 300000
 
-# flag for training mode
-TRAINING = False
 
 # deque for the mean of the rewards measured in the matches
 mean = deque(maxlen=10000)
